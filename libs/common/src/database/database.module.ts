a61941309e5e789a5bce0532a236e9762ab676c4
mxshahan <mx.shahan@gmail.com>
2024-04-03 14:46:35 +0600

    db updated

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -4,25 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import getMigragions from './migration';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: (configService: ConfigService) => {
-        return {
-          type: 'postgres',
-          host: configService.get('POSTGRES_HOST'),
-          port: configService.get('POSTGRES_PORT'),
-          username: configService.get('POSTGRES_USER'),
-          password: configService.get('POSTGRES_PASSWORD'),
-          database: configService.get('POSTGRES_DB'),
-          logging: false,
-          synchronize: true,
-          migrations: getMigragions(),
-          autoLoadEntities: true,
-        };
-      },
-      inject: [ConfigService],
-    }),
-  ],
+  // imports: [
+  //   TypeOrmModule.forRootAsync({
+  //     useFactory: (configService: ConfigService) => {
+  //       return {
+  //         type: 'postgres',
+  //         host: configService.get('POSTGRES_HOST'),
+  //         port: configService.get('POSTGRES_PORT'),
+  //         username: configService.get('POSTGRES_USER'),
+  //         password: configService.get('POSTGRES_PASSWORD'),
+  //         database: configService.get('POSTGRES_DB'),
+  //         logging: false,
+  //         synchronize: true,
+  //         migrations: getMigragions(),
+  //         autoLoadEntities: true,
+  //       };
+  //     },
+  //     inject: [ConfigService],
+  //   }),
+  // ],
 })
 export class DatabaseModule {
   static switchMySQL(): DynamicModule {
