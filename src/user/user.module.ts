20ab61e810c6d9d868246eb42cfe5c86c03f95f3
mxshahan <mx.shahan@gmail.com>
2024-06-05 16:42:55 +0600

    updated module

---
+++ src/user/user.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
+import { AzureFilesService, Connection, User } from '@app/common';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Connection])],
+  providers: [UserResolver, UserService, AzureFilesService],
+  exports: [TypeOrmModule, UserService],
+})
+export class UserModule {}
+
+// Module Added
