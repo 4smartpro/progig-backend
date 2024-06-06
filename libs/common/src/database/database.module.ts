6d6a30042581364988ae7c712f3c67d698539b68
mxshahan <mx.shahan@gmail.com>
2024-06-07 01:42:20 +0600

    Updated database

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import getMigragions from './migration';
@@ -8,8 +8,12 @@ import getMigragions from './migration';
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
         return {
-          type: 'mysql',
-          url: configService.get('MYSQL_DATABASE_URL'),
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
           logging: false,
           synchronize: true,
           migrations: getMigragions(),
@@ -20,27 +24,25 @@ import getMigragions from './migration';
     }),
   ],
 })
-export class DatabaseModule {}
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRootAsync({
-//       useFactory: (configService: ConfigService) => {
-//         return {
-//           type: 'postgres',
-//           host: configService.get('POSTGRES_HOST'),
-//           port: configService.get('POSTGRES_PORT'),
-//           username: configService.get('POSTGRES_USER'),
-//           password: configService.get('POSTGRES_PASSWORD'),
-//           database: configService.get('POSTGRES_DB'),
-//           logging: false,
-//           synchronize: true,
-//           migrations: getMigragions(),
-//           autoLoadEntities: true,
-//         };
-//       },
-//       inject: [ConfigService],
-//     }),
-//   ],
-// })
-// export class DatabaseModule {}
+export class DatabaseModule {
+  static switchMySQL(): DynamicModule {
+    return {
+      module: DatabaseModule,
+      imports: [
+        TypeOrmModule.forRootAsync({
+          useFactory: (configService: ConfigService) => {
+            return {
+              type: 'mysql',
+              url: configService.get('MYSQL_DATABASE_URL'),
+              logging: false,
+              synchronize: true,
+              migrations: getMigragions(),
+              autoLoadEntities: true,
+            };
+          },
+          inject: [ConfigService],
+        }),
+      ],
+    };
+  }
+}
